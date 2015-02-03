d7a5aab8c9def316324ef2ac9a84f7ddbef832ec
Jeremy Hageman <jjhageman@gmail.com>
2015-02-02 23:12:50 -0800

    init

--- Rotten Tomatoes/ViewController.swift
+++ Rotten Tomatoes/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDataSource {
+    override func overrideTraitCollectionForChildViewController(childViewController: UIViewController) -> UITraitCollection! {
+        <#code#>
+    } func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(<#identifier: String#>)
+        let cell = UITableViewCell(style: <#UITableViewCellStyle#>, reuseIdentifier: nil)
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        return cell
     }
-
 
 }
 
